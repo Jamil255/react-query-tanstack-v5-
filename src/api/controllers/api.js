a2572ccb21b24139f7db3b3f7b51c50cc3b8fe31
unknown <jamilafzal255@gmail.com>
2024-06-24 15:06:41 +0500

    api calling

--- src/api/controllers/api.js
+++ src/api/controllers/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const fetchPost = async () => {
-  const response = await axios.get('http://localhost:3000/posts')
+  const response = await axios.get('http://localhost:3000/posts?_sort=-id')
   return response
 }
 
-export { fetchPost } 
+export { fetchPost }
